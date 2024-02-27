f4ef1238f3f56ae031c56b74fd116c4a9cd89dc7
Mukhametzhan <zhanbekovm@mail.ru>
2024-02-27 22:51:58 +0500

    add: Routes add base routingr

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
-
+import Header from './Layout/Header/Header'
+import SideBar from './Layout/SideBar/SideBar'
+import {Routes,Route} from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Authorization from './Pages/Authorization/Authorization';
+import Home from './Pages/Home/Home';
 
 function App() {
 
   return (
     <>
-    
+          <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route path="/auth" element={<Authorization/>}/>
+              <Route path="/home" element={<Home/>}/>
+            </Route>
+          </Routes>
     </>
   )
 }
